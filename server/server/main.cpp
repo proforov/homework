b0f01a79fd8307e9da635cdd8fd308535d40737b
proforov <proforov@gmail.com>
2016-05-17 00:25:49 +0300

    server stub, worklog #1h 30m

---
+++ server/server/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  server
+//
+//  Created by Sergey Proforov on 17.05.16.
+//  Copyright (c) 2016 Proforov Inc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
